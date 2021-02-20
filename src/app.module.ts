cf9bb76ecf299414e8d3c243a6c1d50fd095a1ca
ithkkeii <ithkkeii@gmail.com>
2021-02-20 23:38:46 +0700

    feat: docker-compose enhanced config.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 
+console.log(process.env.DATABASE_HOST);
+
 @Module({
-  imports: [AuthModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      migrationsTableName: 'migration',
+      migrations: ['dist/migration/*.js'],
+      cli: {
+        migrationsDir: 'src/migration',
+      },
+      logging: 'all',
+    }),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
