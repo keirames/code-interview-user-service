cbf79880ff3098c1f363828a52c0d768ff62d6df
ithkkeii <ithkkeii@gmail.com>
2021-02-25 20:38:36 +0700

    feat: Clean nestjs initialize.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,4 @@
 import { Controller } from '@nestjs/common';
-import { MessagePattern } from '@nestjs/microservices';
-import { AppService } from './app.service';
 
 @Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @MessagePattern({ cmd: 'sum' })
-  sum(data: number[]) {
-    return (data || []).reduce((a, b) => a + b);
-  }
-}
+export class AppController {}
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     { transport: Transport.TCP, options: { host: '0.0.0.0', port: 3000 } },
