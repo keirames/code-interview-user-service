660605a6e42e95433917f69bd3c4c2e53a8a5849
ithkkeii <ithkkeii@gmail.com>
2021-03-01 22:56:40 +0700

    feat: Config connect to external kafka broker.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,8 +3,8 @@ import { MessagePattern } from '@nestjs/microservices';
 
 @Controller()
 export class AuthController {
-  @MessagePattern({ cmd: 'validateUser' })
-  async validateUser(): Promise<string> {
-    return 'not available';
-  }
+  // @MessagePattern({ cmd: 'validateUser' })
+  // async validateUser(): Promise<string> {
+  //   return 'not available';
+  // }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,32 @@
 import { NestFactory } from '@nestjs/core';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    { transport: Transport.TCP, options: { host: '0.0.0.0', port: 3000 } },
-  );
-  app.listen(() => console.log('AuthService is listening.'));
+  const app = await NestFactory.create(AppModule);
+
+  // Microservice TCP
+  const microserviceTCP = app.connectMicroservice({
+    transport: Transport.TCP,
+    options: { host: '0.0.0.0', port: 3001 },
+  });
+
+  // Microservice kafka
+  const microserviceKafka = app.connectMicroservice({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        clientId: 'hero',
+        brokers: [`${process.env.KAFKA_HOST}:9092`],
+      },
+      consumer: {
+        groupId: 'hero-consumer',
+      },
+    },
+  });
+
+  await app.startAllMicroservicesAsync();
+  await app.listen(3000);
+  console.log(`Listening on port ${3000}`);
 }
 bootstrap();
